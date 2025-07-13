56aefc2f064b9830fca1777ec8eba0fa618b10c8
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2025-07-14 01:10:55 +0530

    add unit test

---
+++ demo/src/utils/date-utils.test.js
@@ -0,0 +1,22 @@
+import { currentDate, formattedTime } from "./date-utils";
+
+describe("formattedTime", () => {
+  it("formats time correctly for 0ms", () => {
+    expect(formattedTime(0)).toBe("00:00.00");
+  });
+
+  it("formats time correctly for 1 minute, 2 seconds, 340ms", () => {
+    const result = formattedTime(62340);
+    expect(result).toBe("01:02.34");
+  });
+
+  it("formats time with leading zeros", () => {
+    const result = formattedTime(904);
+    expect(result).toBe("00:00.90");
+  });
+
+  it("formats exactly 1 hour correctly", () => {
+    const result = formattedTime(3600000);
+    expect(result).toBe("00:00.00");
+  });
+});
