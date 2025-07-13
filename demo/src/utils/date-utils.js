650e7b4af86fe9ef8a00da33841cafc9cc93d957
Athul Antony <162117226+athuldev2024@users.noreply.github.com>
2025-07-13 18:24:14 +0530

    add new time function

--- demo/src/utils/date-utils.js
+++ demo/src/utils/date-utils.js
@@ -1,3 +1,13 @@
 export const currentDate = () => {
   return new Date().getFullYear();
 };
+
+export const formattedTime = (time) => {
+  const getTwoDigits = (num) => String(num).padStart(2, "0");
+
+  const minutes = getTwoDigits(Math.floor(time / 60000) % 60);
+  const seconds = getTwoDigits(Math.floor(time / 1000) % 60);
+  const milliseconds = getTwoDigits(Math.floor((time % 1000) / 10));
+
+  return `${minutes}:${seconds}.${milliseconds}`;
+};
